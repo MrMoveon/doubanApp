17825eb1db2e49a7c3b37a8bbeb2b2aa03dff983
530385371@qq.com <3536024>
2017-02-27 19:46:49 +0800

    删除无用的信息

--- src/main.js
+++ src/main.js
@@ -3,13 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-//import './assets/css/app.css';
 import './assets/less/app.less';
-// import Mint from 'mint-ui';
-//import 'mint-ui/lib/style.css'
-// Vue.use(Mint);
-// import { Cell } from 'mint-ui';
-// Vue.component(Cell.name, Cell);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
